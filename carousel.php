53452ef263918ac04fe817f879845d18cd4145e3
Karina Tapia <mitzyrinami@hotmail.com>
2013-07-19 03:23:43 -0500

    carrousel

---
+++ carousel.php
@@ -0,0 +1,23 @@
+<!-- carrousel-->
+        <div id="carusel" style="width:600px; height:258px; background:#036; position:absolute; z-index:999999">
+        
+         <div class="gallery-holder">
+                        	<div class="gallery_slide" id="gallery_sec1" >
+                        		<div class="holder">
+                                	<div class="list">
+                                    <div id="Gallery2" class="gallery">
+                                    <div class="item_s"><div class="box_s box1"><a href="img/fotos/13.jpg"><img src="img/fotos/13.jpg" width="210" height="159"/></a></div></div>
+                                    <div class="item_s"><div class="box_s box1"><a href="img/fotos/13.jpg"><img src="img/fotos/13.jpg" width="210" height="159"/></a></div></div>
+                                    <div class="item_s"><div class="box_s box1"><a href="img/fotos/13.jpg"><img src="img/fotos/13.jpg" width="210" height="159"/></a></div></div>
+                                    <div class="item_s"><div class="box_s box1"><a href="img/fotos/13.jpg"><img src="img/fotos/13.jpg" width="210" height="159"/></a></div></div>
+                                    <div class="item_s"><div class="box_s box1"><a href="img/fotos/13.jpg"><img src="img/fotos/13.jpg" width="210" height="159"/></a></div></div>
+                                    <div class="item_s"><div class="box_s box1"><a href="img/fotos/13.jpg"><img src="img/fotos/13.jpg" width="210" height="159"/></a></div></div>
+                                    <div class="item_s"><div class="box_s box1"><a href="img/fotos/13.jpg"><img src="img/fotos/13.jpg" width="210" height="159"/></a></div></div>
+                                    <div class="item_s"><div class="box_s box1"><a href="img/fotos/13.jpg"><img src="img/fotos/13.jpg" width="210" height="159"/></a></div></div>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        </div>
+       <!-- carrousel--> 
